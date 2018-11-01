3eac372a3da800c62d3422783d8474e14171449f
Sean Joyal <sean.joyal@gmail.com>
2018-10-31 21:32:11 -0400

    add dispatch wrapping util

--- src/reducers/utils/wrapDispatch.js
+++ src/reducers/utils/wrapDispatch.js
@@ -1,1 +1,8 @@
-export default function wrapDispatch() {}
+// NOTE: this isn't currently used
+// it could be used in correlation with a mapping function to eliminate boilerplate
+// passing dispatch and actions down as props
+export default function wrapDispatch(action, dispatch) {
+  return payload => {
+    dispatch(action(payload))
+  }
+}
